d76239890ea272944d6c634193070943d18564ac
TonyPnode <sgtp@me.com>
2019-10-28 00:46:50 -0400

    Initial Commit /AJP//

---
+++ src/components/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a todolist thing in react, doing some stuff</p>
+        </React.Fragment>
+    )
+
+}
+
+export default About
